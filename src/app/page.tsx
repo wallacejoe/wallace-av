bd6c10426c55b8c4cbf9eb5187f91d5f00e8f3eb
wallacejoe <JAWcoder@gmail.com>
2025-04-23 14:37:42 -0700

    Added flexible styling to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ export default function Page() {
       <MainNav />
       <section className="w-full flex flex-col items-center p-12 box-border text-center">
         <h2 className="w-6/12 text-4xl">
-          From Events & Conferences to backyard Parties we have you covered with
+          From events & conferences to backyard parties we have you covered with
           professional equipment and service
         </h2>
       </section>
-      <div className="w-full flex justify-around p-12 box-border mb-12 mt-8">
-        <p className="w-5/12 text-3xl">
+      <div className="w-full flex flex-col lg:flex-row lg:justify-around p-12 box-border mb-12 mt-8">
+        <p className="lg:w-5/12 text-3xl">
           Serving in and around the Mesa Arizona area we can deliver, setup, and
           be onsite for technical assistance.
         </p>
-        <section>
+        <section className="max-lg:w-full max-lg:mt-12 max-lg:text-center">
           <h3 className="mb-5 text-lg">Contact Us</h3>
           <ul className="text-lg">
             <li>Sales@WallaceAV.com</li>
