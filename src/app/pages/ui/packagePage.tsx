3ec0b303536084a437e21ac6ed7840a71e62bafd
wallacejoe <JAWcoder@gmail.com>
2025-04-25 14:55:40 -0700

    Added some flexibility to the product/package page

--- src/app/pages/ui/packagePage.tsx
+++ src/app/pages/ui/packagePage.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 export default async function PackagePage() {
   return (
-    <div className="w-3/4 grid grid-cols-3 gap-10 justify-items-center">
+    <div className="lg:w-3/4 lg:grid lg:grid-cols-3 gap-10 justify-items-center">
       {packages.map((item) => (
         <Link
           href={`#` /*`/dashboard/packages/${package.package_id}`*/}
           // Link will be added to/with individual package pages
           key={item.package_id}
-          className="w-fit p-2 flex bg-neutral-500 rounded-lg justify-center box-border"
+          className="max-lg:mb-8 w-fit p-2 flex bg-neutral-500 rounded-lg justify-center box-border"
         >
           <div>
             <div className="w-full h-24 mb-3 bg-orange-300"></div>
