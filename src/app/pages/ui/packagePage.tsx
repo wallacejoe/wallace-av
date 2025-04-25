9743ddf03988f9c8d192436afbdca0858ec775b8
wallacejoe <JAWcoder@gmail.com>
2025-04-25 14:45:53 -0700

    Populated packages page will placeholder data

--- src/app/pages/ui/packagePage.tsx
+++ src/app/pages/ui/packagePage.tsx
@@ -1,5 +1,24 @@
+import { packages } from "@/app/lib/placeholder-data";
+import Link from "next/link";
 import React from "react";
 
 export default async function PackagePage() {
-  return <div></div>;
+  return (
+    <div className="w-3/4 grid grid-cols-3 gap-10 justify-items-center">
+      {packages.map((item) => (
+        <Link
+          href={`#` /*`/dashboard/packages/${package.package_id}`*/}
+          // Link will be added to/with individual package pages
+          key={item.package_id}
+          className="w-fit p-2 flex bg-neutral-500 rounded-lg justify-center box-border"
+        >
+          <div>
+            <div className="w-full h-24 mb-3 bg-orange-300"></div>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
