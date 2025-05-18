a874e007fad935ef6a53941911e4f8b6e2e4c225
wallacejoe <JAWcoder@gmail.com>
2025-05-18 16:00:12 -0700

    Styling changes to the cart page.

--- src/app/pages/cart/page.tsx
+++ src/app/pages/cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import EmailForm from "@/components/common/emailForm";
 import { getFromStorage, removeFromStorage } from "@/utilities/localStorage";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Page() {
@@ -15,12 +16,23 @@ export default function Page() {
   }, []);
 
   return (
-    <div>
+    <div className="mt-2 w-full flex flex-col justify-items-center">
       {storage.length > 0 ? (
         <div className="w-full flex flex-col items-center">
           {storage.map((item: any) => (
-            <div key={item.item.productId}>
-              <p>{item.item.name}</p>
+            <div
+              className="mb-4 w-fit p-2 flex flex-col justify-center box-border bg-neutral-500 rounded-lg"
+              key={item.item.productId}
+            >
+              <Link
+                href={`/pages/products/${item.item.productId}`}
+                // Link will be added to/with individual product pages
+                key={item.item.productId}
+                className="w-full rounded-lg p-2 box-border"
+              >
+                <div className="w-full h-24 bg-orange-300 rounded-lg"></div>
+              </Link>
+              <h2>{item.item.name}</h2>
               <button onClick={() => removeFromStorage(item.item.productId)}>
                 Remove Item
               </button>
